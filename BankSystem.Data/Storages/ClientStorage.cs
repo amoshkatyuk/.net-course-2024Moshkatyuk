6da1cd9f5a0a21257ff1306d054cb55c5729d697
Александр Мошкатюк <amoshkatyuk@gmail.com>
2024-10-01 19:27:18 +0300

    Добавлены классы ClientStorage, EmployeeStorage. Реализованы тесты вышеперечисленных классов.

---
+++ BankSystem.Data/Storages/ClientStorage.cs
@@ -0,0 +1,44 @@
+﻿using BankSystem.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankSystem.Data.Storages
+{
+    public class ClientStorage
+    {
+        private List<Client> _clients;
+
+        public ClientStorage()
+        {
+            _clients = new List<Client>();
+        }
+
+        public void AddClients(List<Client> clients)
+        {
+            _clients.AddRange(new List<Client>());
+        }
+
+        public void AddClient(Client client) 
+        {
+            _clients.Add(client);
+        }
+
+        public Client GetYoungestClient() 
+        {
+            return _clients.OrderBy(c => c.Age).FirstOrDefault();
+        }
+
+        public Client GetOldestClient() 
+        {
+            return _clients.OrderBy(c => c.Age).LastOrDefault();
+        }
+
+        public double GetClientsAverageAge() 
+        {
+            return _clients.Average(c => c.Age);
+        }
+    }
+}
