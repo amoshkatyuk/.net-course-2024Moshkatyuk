9006ab61c8ac360ad27bf41c9c4d2618704bd01a
Александр Мошкатюк <amoshkatyuk@gmail.com>
2024-10-20 21:34:49 +0300

    Недочеты устранены

--- BankSystem.ExportTool.Tests/ExportServiceTests.cs
+++ BankSystem.ExportTool.Tests/ExportServiceTests.cs
@@ -12,9 +12,9 @@ namespace BankSystem.ExportTool.Tests
         private readonly ClientService _clientService;
         private readonly ClientStorage _clientStorage;
         private readonly TestDataGenerator _testDataGenerator;
-        private readonly ExportService _exportService;
+        private readonly ExportService<Client> _exportService;
         private readonly string _testDirectory = "TestCsvDirectory";
-        private readonly string _csvFileName = "test—lients.csv";
+        private readonly string _csvFileName = "testClients.csv";
 
         public ExportServiceTests()
         {
@@ -22,46 +22,29 @@ namespace BankSystem.ExportTool.Tests
             _testDataGenerator = new TestDataGenerator();
             _clientStorage = new ClientStorage(_context);
             _clientService = new ClientService(_clientStorage);
-            _exportService = new ExportService(_testDirectory, _csvFileName, _clientService, _clientStorage);
+            _exportService = new ExportService<Client>();
         }
 
         [Fact]
         public void ExportClientDataInCsvShouldExportClientDataInCsv()
         {
-            var clients = new List<Client>
-            {
-                new Client
-                {
-                Name = "Alex",
-                Surname = "Ivanov",
-                PassportData = "AA123456789",
-                BirthDate = DateTime.UtcNow.AddYears(-25),
-                TelephoneNumber = "1234567890"
-                },
-
-                new Client
-                {
-                Name = "Dmitry",
-                Surname = "Petrov",
-                PassportData = "AB123456789",
-                BirthDate = DateTime.UtcNow.AddYears(-25),
-                TelephoneNumber = "1234567890"
-                }
-            };
+            var clients = new List<Client>();
+            clients.Add(_testDataGenerator.GenerateClient());
+            clients.Add(_testDataGenerator.GenerateClient());
 
             foreach (var client in clients)
             {
                 _clientService.AddClient(client);
             }
 
-            _exportService.ExportClientDataInCsv(clients);
+            _exportService.ExportDataInCsv(clients, _testDirectory, _csvFileName);
 
             string filePath = Path.Combine(_testDirectory, _csvFileName);
             Assert.True(File.Exists(filePath));
 
             var fileContent = File.ReadAllText(filePath);
-            Assert.Contains("Alex", fileContent);
-            Assert.Contains("Dmitry", fileContent);
+            Assert.Contains(clients[0].Name, fileContent);
+            Assert.Contains(clients[1].Name, fileContent);
 
             foreach (var client in clients)
             {
@@ -72,57 +55,34 @@ namespace BankSystem.ExportTool.Tests
         [Fact]
         public void ImportClientDataFromCsvShouldImportClientDataFromCsv()
         {
-            var csvContent = "TelephoneNumber,Id,Name,Surname,PassportData,BirthDate,Age\r\n" +
-            "1234567890,4436ffff-b080-427f-a597-65529fb9d6ee,Alex,Ivanov,AA123456789,1999-10-18T16:12:39Z,25\r\n" +
-            "1234567890,b1e6bddd-4b36-4a3a-b03d-bd1974df29c6,Dmitry,Petrov,AB123456789,1999-10-18T16:12:39Z,25\r\n";
+            var clients = new List<Client>();
+            clients.Add(_testDataGenerator.GenerateClient());
+            clients.Add(_testDataGenerator.GenerateClient());
+
+            foreach (var client in clients)
+            {
+                _clientService.AddClient(client);
+            }
+
+            _exportService.ExportDataInCsv(clients, _testDirectory, _csvFileName);
 
             string filePath = Path.Combine(_testDirectory, _csvFileName);
-            Directory.CreateDirectory(_testDirectory);
-            File.WriteAllText(filePath, csvContent);
+            Assert.True(File.Exists(filePath));
             
-            _exportService.ImportClientDataFromCsv();
+            _exportService.ImportDataFromCsv(_testDirectory, _csvFileName);
 
-            var clients = _context.Clients.ToList();
+            var importedClients = _context.Clients.ToList();
+
             Assert.Equal(2, clients.Count);
-            Assert.Contains(clients, c => c.PassportData == "AA123456789");
-            Assert.Contains(clients, c => c.PassportData == "AB123456789");
+            foreach (var client in clients)
+            {
+                Assert.Contains(importedClients, c => c.PassportData == client.PassportData);
+            }
 
             foreach (var client in clients)
             {
                 _clientService.DeleteClient(client.Id);
             }
-        }
-
-        [Fact]
-        public void ImportClientDataFromCsvShouldUpdateExistingClients()
-        {
-            var client = new Client
-            {
-                Id = Guid.Parse("14204f92-1ddf-4b90-b8bd-fd3a5fd5809d"),
-                Name = "Alex",
-                Surname = "Ivanov",
-                PassportData = "AA123456987",
-                BirthDate = DateTime.UtcNow.AddYears(-25),
-                TelephoneNumber = "1234567890"
-            };
-            _clientService.AddClient(client);
-
-            var csvContent = "TelephoneNumber,Id,Name,Surname,PassportData,BirthDate,Age\r\n" +
-            "1234567890,14204f92-1ddf-4b90-b8bd-fd3a5fd5809d,Alex,Aleksandrov,AA123456789,1999-10-18T16:12:39Z,25\r\n";
-
-            string filePath = Path.Combine(_testDirectory, _csvFileName);
-            Directory.CreateDirectory(_testDirectory);
-            File.WriteAllText(filePath, csvContent);
-
-            _exportService.ImportClientDataFromCsv();
-
-            var updatedClient = _clientStorage.GetById(client.Id);
-
-            Assert.NotNull(updatedClient);
-            Assert.Equal("AA123456789", updatedClient.PassportData);
-            Assert.Equal("Aleksandrov", updatedClient.Surname);
-
-            _clientService.DeleteClient(updatedClient.Id);
         }
 
         [Fact]
@@ -134,7 +94,7 @@ namespace BankSystem.ExportTool.Tests
                 File.Delete(filePath);
             }
 
-            var exception = Assert.Throws<FileNotFoundException>(() => _exportService.ImportClientDataFromCsv());
+            var exception = Assert.Throws<FileNotFoundException>(() => _exportService.ImportDataFromCsv(_testDirectory, _csvFileName));
             Assert.Equal("‘‡ÈÎ ‰Îˇ ËÏÔÓÚ‡ ÌÂ Ì‡È‰ÂÌ", exception.Message);
         }
     }
--- ExportTool/ExportService.cs
+++ ExportTool/ExportService.cs
@@ -7,31 +7,18 @@ using System.Globalization;
 
 namespace ExportTool
 {
-    public class ExportService
+    public class ExportService<T> where T: class
     {
-        private string _pathToDicrectory { get; set; }
-        private string _csvFileName { get; set; }
 
-        private readonly ClientService _clientService;
-        private readonly ClientStorage _clientStorage;
-
-        public ExportService(string pathToDirectory, string csvFileName, ClientService clientService, ClientStorage clientStorage)
+        public void ExportDataInCsv(IEnumerable<T> entities, string pathToDirectory, string csvFileName) 
         {
-            _pathToDicrectory = pathToDirectory;
-            _csvFileName = csvFileName;
-            _clientService = clientService;
-            _clientStorage = clientStorage;
-        }
-
-        public void ExportClientDataInCsv(List<Client> clients) 
-        {
-            DirectoryInfo dirInfo = new DirectoryInfo(_pathToDicrectory);
+            DirectoryInfo dirInfo = new DirectoryInfo(pathToDirectory);
             if (!dirInfo.Exists) 
             {
                 dirInfo.Create();
             }
 
-            string fullPath = Path.Combine(_pathToDicrectory, _csvFileName);
+            string fullPath = Path.Combine(pathToDirectory, csvFileName);
 
             using (FileStream fileStream = new FileStream(fullPath, FileMode.OpenOrCreate)) 
             {
@@ -39,17 +26,15 @@ namespace ExportTool
                 {
                     using (CsvWriter csvWriter = new CsvWriter(streamWriter, CultureInfo.InvariantCulture)) 
                     {
-                        csvWriter.WriteRecords(clients);
-
-                        csvWriter.Flush();
+                        csvWriter.WriteRecords(entities);
                     }
                 }
             }
         }
 
-        public void ImportClientDataFromCsv() 
+        public IEnumerable<T> ImportDataFromCsv(string pathToDirectory, string csvFileName) 
         {
-            string fullpath = Path.Combine(_pathToDicrectory, _csvFileName);
+            string fullpath = Path.Combine(pathToDirectory, csvFileName);
 
             if (!File.Exists(fullpath))
             {
@@ -62,23 +47,7 @@ namespace ExportTool
                 {
                     using (CsvReader csvReader = new CsvReader(streamReader, CultureInfo.InvariantCulture)) 
                     {
-                        var clients = csvReader.GetRecords<Client>().ToList();
-
-                        foreach (var client in clients)
-                        {
-                            client.BirthDate = DateTime.SpecifyKind(client.BirthDate, DateTimeKind.Utc);
-
-                            var existingClient = _clientStorage.GetById(client.Id); // использую метод из Storage, т.к. в Serivce у нас логика выброса исключения
-
-                            if (existingClient != null)
-                            {
-                                _clientStorage.Update(client.Id, client);
-                            }
-                            else
-                            {
-                                _clientService.AddClient(client);
-                            }
-                        }
+                        return csvReader.GetRecords<T>().ToList();
                     }
                 }
             }
